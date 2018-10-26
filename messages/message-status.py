caa787f7a320e994352c5a5315b8e5cdc17b551f
Lourens Touwen <touwenlourens@gmail.com>
2018-10-26 23:58:53 +0200

    added docstring to message_status()

--- messages/message-status.py
+++ messages/message-status.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 @app.route("/webhooks/message-status", methods=['POST'])
 def message_status():
+    #print message status and returns status code "200" 
     data = request.get_json()
     pprint(data)
     return "200"
